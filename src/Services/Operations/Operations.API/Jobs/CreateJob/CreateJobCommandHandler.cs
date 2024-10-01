1d694635e3fda74ab2deb7f51e2ac56684ce86c7
Mark Baines <markbaines121@hotmail.com>
2024-10-01 13:42:33 +0100

    test change

--- src/Services/Operations/Operations.API/Jobs/CreateJob/CreateJobCommandHandler.cs
+++ src/Services/Operations/Operations.API/Jobs/CreateJob/CreateJobCommandHandler.cs
@@ -10,6 +10,7 @@ public record CreateJobResult(Guid id);
 public class CreateJobCommandHandler(IJobRepository _jobRepository) 
     : ICommandHandler<CreateJobCommand, CreateJobResult>
 {
+    // handle
     public async Task<CreateJobResult> Handle(CreateJobCommand request, CancellationToken cancellationToken)
     {
         var jobId = await _jobRepository.CreateJob(request.job, cancellationToken);
