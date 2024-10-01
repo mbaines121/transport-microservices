e45f62d0ee0b4c1007fb7238bfff1a5be039e748
Mark Baines <markbaines121@hotmail.com>
2024-10-01 13:34:54 +0100

    updated pipelines and added suppliers api

--- src/Services/Suppliers/Suppliers.API/Program.cs
+++ src/Services/Suppliers/Suppliers.API/Program.cs
@@ -1,6 +1,18 @@
+using Carter;
+using HealthChecks.UI.Client;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
+
+app.UseHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
